64c899d1d4dcea0c2b16e0ad7c6a17a8918e1a34
SMTKKAUNG23 <144818747+SMTKKAUNG23@users.noreply.github.com>
2023-09-14 00:50:57 +0630

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,24 +17,24 @@ none='\e[0m'
 
 sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_404/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
 echo "
-<h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
+<h3><font color='#F50057'>▬▬▬▬▬▬※¤¤¤※▬▬▬▬▬▬
 </font></h3>
-<h3><font color='magenta'>--- ۩ PREMIUM SERVER ۩ ---
-</font></h3>
-
-<h3><font color='green'>--- ۩ SERVER BY ④ ⍤⃝🌻④ ۩ ---
+<h3><font color='#00E676'>--- ♥ PREMIUM SERVER ♥ ---
 </font></h3>
 
-<h4><font color='cyan'>Telegram Channel >> https://t.me/Pmttg
+<h3><font color='#FFEA00'>--- ♥ SERVER BY 🅢🅜🅣 ♥ ---
+</font></h3>
+
+<h4><font color='#01579B'>ကလိ ⓑⓨ 🅂🄼🅃
 </font></h4>
 
-<h4><font color='blue'>Telegram Channel >> https://t.me/nkka_404
+<h4><font color='#2E7D32'>You are what you do, not what you say you will do.
 </font></h4>
 
-<h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
+<h3><font color='#F50057'>▬▬▬▬▬▬※¤¤¤※▬▬▬▬▬▬
 </font></h3>" | tee /etc/ssh/gcp_404 >/dev/null
-useradd "404" --shell=/bin/false -M
-echo "404:404" | chpasswd
+useradd "smt23" --shell=/bin/false -M
+echo "smt23:smt23" | chpasswd
 
 echo ""
 
@@ -49,4 +49,4 @@ echo -e "${cyan}          \|__|        \|_______|              \|__|            
 echo -e "${green}Contact the developer https://t.me/nkka404 for more information              ${plain}"
 echo -e "${yellow}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
 
-echo -e "${cyan}i am 404 😎 ${plain}"
+echo -e "${cyan}i am 🆂🅼🆃 😎 ${plain}"
